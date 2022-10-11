6642b0e8afd36521cbb88df571ce70b6f15836d1
ekukwa <easmsm@gmail.com>
2022-10-11 10:47:42 -0400

    trying to resolve server connection error, I think it's something to do with the .env file, but saving changes I've made along the way (syntax inconsistency fixes) now

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -48,7 +48,7 @@ router.get('/:id', (req, res) => {
   })
     .then(dbProductData => {
       if (!dbProductData) {
-        res.status(404).json({ message: 'No product found with this id' });
+        res.status(404).json({ message: 'No product found.' });
         return;
       }
       res.json(dbProductData);
@@ -130,7 +130,7 @@ router.delete('/:id', (req, res) => {
   })
     .then(dbProductData => {
       if (!dbProductData) {
-        res.status(404).json({ message: 'No product found with this id' });
+        res.status(404).json({ message: 'No product found.' });
         return;
       }
       res.json(dbProductData);
