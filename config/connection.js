75a6bd6b5fd5f0850d7f5b482edd1fa93c829583
ekukwa <easmsm@gmail.com>
2022-10-15 15:21:42 -0400

    resolved error that was preventing seeding, but I still can't get/push/delete on insomnia

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,6 @@
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
-const { Sequelize } = require("sequelize/types");
-
-// require('dotenv').config();
-
-
-// // const sequelize = new Sequelize('just_tech_news_db', 'root', '76Te*omcYh8qXXSO9ro45GjxK3ZaudQ1D!', {
-// //   host: 'localhost',
-// //   dialect: 'mysql',
-// //   port: 3306
-// // });
-
-//  const sequelize = process.env.JAWSDB_URL
-//    ? new Sequelize(process.env.JAWSDB_URL)
-//    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//        host: 'localhost',
-//        dialect: 'mysql',
-//        dialectOptions: {
-//          decimalNumbers: true,
-//        },
-//      });
+require('dotenv').config();
 
 let sequelize;
 
